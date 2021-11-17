09c82acea36c69a7170b257b555a7a0549c03c7f
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-17 13:05:55 -0500

    Spelling corrected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: `http://localhost:7007`,
+  apiUrl: `https://app-kimsa.herokuapp.com/api`,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: `http://localhost:7007/api`,
+  apiUrl: `https://app-kimsa.herokuapp.com/api`,
 };
